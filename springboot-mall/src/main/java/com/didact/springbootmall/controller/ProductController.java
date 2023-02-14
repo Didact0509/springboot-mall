9430016a2ad7e4a2c75386b3fcc3b10ab9ee9140
Didact0509 <momo22732375@gmail.com>
2023-02-14 11:21:01 +0800

    商品功能 - 實作修改商品

--- springboot-mall/src/main/java/com/didact/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/didact/springbootmall/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.didact.springbootmall.dto.ProductRequest;
 import com.didact.springbootmall.model.Product;
 import com.didact.springbootmall.service.ProductService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -33,5 +34,23 @@ public class ProductController {
         Product product = productService.getProductById(productId);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
+    }
+
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+                                                 @RequestBody @Valid ProductRequest productRequest) {
+        // 檢查商品是否存在
+        Product product = productService.getProductById(productId);
+
+        if (product == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        // 修改商品數據
+        productService.updateProduct(productId, productRequest);
+
+        Product updatedProduct = productService.getProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
     }
 }
