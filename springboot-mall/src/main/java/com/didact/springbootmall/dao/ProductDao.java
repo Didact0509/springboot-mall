345864b4a4e1ff131aec84c9e2b16b15e7fae703
Didact0509 <momo22732375@gmail.com>
2023-02-14 10:36:19 +0800

    商品功能 - 實作新增商品

--- springboot-mall/src/main/java/com/didact/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/didact/springbootmall/dao/ProductDao.java
@@ -1,8 +1,11 @@
 package com.didact.springbootmall.dao;
 
+import com.didact.springbootmall.dto.ProductRequest;
 import com.didact.springbootmall.model.Product;
 
 public interface ProductDao {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
--- springboot-mall/src/main/java/com/didact/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/didact/springbootmall/service/ProductService.java
@@ -1,9 +1,12 @@
 package com.didact.springbootmall.service;
 
 import com.didact.springbootmall.dao.ProductDao;
+import com.didact.springbootmall.dto.ProductRequest;
 import com.didact.springbootmall.model.Product;
 
 public interface ProductService {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
