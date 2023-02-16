6db5cb6da23fea16135aabbfd385660f4d14d2b4
Didact0509 <momo22732375@gmail.com>
2023-02-16 16:40:11 +0800

    訂單功能 - 創建訂單(上)

---
+++ springboot-mall/src/main/java/com/didact/springbootmall/dao/OrderDao.java
@@ -0,0 +1,12 @@
+package com.didact.springbootmall.dao;
+
+import com.didact.springbootmall.model.OrderItem;
+
+import java.util.List;
+
+public interface OrderDao {
+
+    Integer createOrder(Integer userId, Integer totalAmount);
+
+    void createOrderItems(Integer orderId, List<OrderItem> orderItemList);
+}
